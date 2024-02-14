44acc00de505ec9900e96ccabbd16734e499ad33
SatyamDubey <67541381+Satyam-git-hub@users.noreply.github.com>
2024-02-14 11:29:17 +0530

    Update vethfinder.sh

--- vethfinder.sh
+++ vethfinder.sh
@@ -1,11 +1,25 @@
+# #!/bin/bash
+
+# for container in $(docker ps -q); do
+#     container_name=$(docker inspect --format='{{.Name}}' $container | sed 's;/;;')
+#     iflink=$(docker exec -it $container bash -c 'cat /sys/class/net/eth0/iflink')
+#     iflink=$(echo $iflink | tr -d '\r')
+#     veth=$(grep -l $iflink /sys/class/net/veth*/ifindex)
+#     veth=$(echo $veth | sed -e 's;^.*net/\(.*\)/ifindex$;\1;')
+#     ip_addr=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container)
+#     echo "$container_name:$veth $ip_addr $container_name"
+# done
 #!/bin/bash
 
 for container in $(docker ps -q); do
     container_name=$(docker inspect --format='{{.Name}}' $container | sed 's;/;;')
-    iflink=$(docker exec -it $container bash -c 'cat /sys/class/net/eth0/iflink')
-    iflink=$(echo $iflink | tr -d '\r')
-    veth=$(grep -l $iflink /sys/class/net/veth*/ifindex)
-    veth=$(echo $veth | sed -e 's;^.*net/\(.*\)/ifindex$;\1;')
     ip_addr=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container)
-    echo "$container_name:$veth $ip_addr $container_name"
+
+    for interface in $(docker exec $container ls /sys/class/net/); do
+        iflink=$(docker exec -it $container bash -c "cat /sys/class/net/$interface/iflink")
+        iflink=$(echo $iflink | tr -d '\r')
+        veth=$(grep -l $iflink /sys/class/net/veth*/ifindex)
+        veth=$(echo $veth | sed -e 's;^.*net/\(.*\)/ifindex$;\1;')
+        echo "$container_name:$veth $ip_addr $container_name"
+        done
 done
